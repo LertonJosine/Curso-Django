f8151722d66477c10bbc0f2a6ddc2bca465d0d14
Lerton Josine <108260309+LertonJosine@users.noreply.github.com>
2022-08-02 00:04:07 +0200

    configurei o url.py

---
+++ projecto/urls.py
@@ -0,0 +1,35 @@
+"""projecto URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+
+# http request
+def my_view(request):
+    return HttpResponse("hello world")
+    # http response
+
+
+def pagina_inicial(request):
+    return HttpResponse("Pagina inicial")
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('sobre/', my_view),
+    path('', pagina_inicial),
+]
